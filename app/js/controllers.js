6dfca056290a3f319d8370e967f8d8b8c52c3acb
Alex Chen <elkhouri1@gmail.com>
2014-04-17 01:58:34 -0700

    Number of tweets and friends

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,6 +6,12 @@
   app.controller('MainCtrl', function ($scope, UserService) {
     var me = UserService.me();
     $scope.fb = {};
+    $scope.error = {};
+    $scope.tweets = {};
+    $scope.homeTweets = {};
+    $scope.mentions = {};
+    $scope.numTweets = 5;
+    $scope.actualNum = $scope.numTweets;
 
     for (var provider in me) {
       if (me[provider])
@@ -30,29 +36,40 @@
       if (provider === "facebook")
         initFB();
       if (provider === "twitter")
-        initTW();
+        initTW($scope.numTweets);
     }
 
     function initFB() {
-      me.facebook.get("/me").done(function (res) {
-        $scope.$apply(function () {
-          $scope.fb = res;
+      $.when(me.facebook.get("/me"),
+        me.facebook.get('/me/friends'))
+        .done(function (me, friends) {
+          $scope.$apply(function () {
+            $scope.fb = me[0];
+            $scope.numFriends = friends[0].data.length;
+          });
         });
-      });
     }
 
-    function initTW() {
-      $.when(me.twitter.get("/1.1/statuses/user_timeline.json?count=5"),
-        me.twitter.get("/1.1/statuses/home_timeline.json?count=5"),
-        me.twitter.get("/1.1/statuses/mentions_timeline.json?count=5"))
+    function initTW(numTweets) {
+      $.when(me.twitter.get("/1.1/statuses/user_timeline.json?count=" + numTweets),
+        me.twitter.get("/1.1/statuses/home_timeline.json?count=" + numTweets),
+        me.twitter.get("/1.1/statuses/mentions_timeline.json?count=" + numTweets))
         .done(function (tweets, homeTweets, mentions) {
           $scope.$apply(function () {
             $scope.tweets = tweets[0];
             $scope.homeTweets = homeTweets[0];
             $scope.mentions = mentions[0];
+            $scope.actualNum = numTweets;
+            delete $scope.error.text;
+          });
+        })
+        .fail(function (error) {
+          $scope.$apply(function () {
+            $scope.error.text = error.statusText;
           });
         });
     }
+    $scope.getTweets = initTW;
 
   });
 
